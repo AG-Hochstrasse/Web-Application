d11b162862daaef6a11cb8e983a7d9996a80e889
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-09-09 10:25:05 +0000

    Add Account page

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import InsertPeople from './components/InsertPeople';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link as RouterLink, Navigate } from 'react-router-dom';
 import { ThemeProvider, Box } from '@primer/react'
-import AppLogin from './components/AppLogin';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import { Auth } from '@supabase/auth-ui-react';
 import { supabase } from './services/supabaseClient';
@@ -50,10 +49,10 @@ export function App() {
           <Auth
             supabaseClient={supabase}
             appearance={{ theme: ThemeSupa }}
-            providers={["google", "facebook", "github"]}
+            providers={["github"]}
+            showLinks={false}
           />
         </div>
-
       </div>
     );
   }
@@ -76,7 +75,6 @@ export function App() {
         >
           <Routes>
             <Route path="/" element={<AppHome session={session}/>} />
-            <Route path="/login" element={<AppLogin />} />
             <Route path="/people" element={<PeopleTable />} />
             <Route path="/people/:id" element={<PersonDetail />} />
             <Route path="/about" element={<AppAbout />} />
