e98a98217596d08090a9df24c406d4c66a686fb5
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-09-09 15:56:04 +0000

    add insert people

--- src/components/InsertPeople.tsx
+++ src/components/InsertPeople.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 import { supabase } from "../services/supabaseClient";
+import { Person } from "../Person";
+import { PageHeader, Box, IconButton } from '@primer/react';
+import { ArrowLeftIcon } from '@primer/octicons-react';
 
-export default async function InsertPeople({ session }: any) {
-  alert(JSON.stringify(session.user.id))
-  const newData = {
-    //user_id: session.user.id, // You can include the user's ID in your insert data
-    name: 'John Doe',
-    birth: '2023-09-08',
-    death: null,
-  };
-  alert(JSON.stringify(newData))
-  // Insert data into your Supabase table (e.g., 'people')
+async function insert(newData: Person) {
   const { data, error } = await supabase
     .from('people')  // Specify your table name
     .insert(newData); // Insert the new data
-  alert(data)
-  alert(JSON.stringify(error))
-  if (error) {
-    return <div>Error inserting data</div>
-  } else {
-    return <div>Success</div>
-  }
+}
+export default async function InsertPeople({ session }: any) {
+
+  return (
+    <PageHeader>
+      <PageHeader.TitleArea>
+        <PageHeader.LeadingAction><IconButton icon={ArrowLeftIcon} aria-label="Back" variant="invisible" onClick={() => navigate("/")} /></PageHeader.LeadingAction>
+        <PageHeader.Title><Box sx={{ width: 500 }}>Create person</Box></PageHeader.Title>
+      </PageHeader.TitleArea>
+    </PageHeader>
+  )
 }
