3f37d1c5426a634fefaaeb2a10c860200515be47
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-09-10 17:35:13 +0200

    Updated peopletable header

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { supabase } from '../services/supabaseClient';
-import { Octicon, Box, Link, RelativeTime, Stack, Spinner, Text, Button, PageHeader } from '@primer/react';
+import { Octicon, Box, Link, RelativeTime, Stack, Spinner, Text, Button, PageHeader, IconButton } from '@primer/react';
 import { Link as RouterLink, useLocation, useNavigate } from 'react-router-dom';
-import { EyeClosedIcon, RepoIcon } from '@primer/octicons-react';
-import { SkeletonAvatar, SkeletonText } from '@primer/react/drafts';
+import { ArrowRightIcon, EyeClosedIcon, PlusIcon, RepoIcon } from '@primer/octicons-react';
+import { SkeletonAvatar, SkeletonText, Table } from '@primer/react/drafts';
 import { Banner, DataTable } from '@primer/react/experimental';
 
 import StateIcon from './StateIcon';
 
 import { Person } from '../Person';
+import { TableContainer } from '@primer/react/lib-esm/DataTable/Table';
 
 const PeopleTable: React.FC = () => {
   const [data, setData] = useState<any[]>([]);
@@ -65,77 +66,71 @@ const PeopleTable: React.FC = () => {
 
   return (
     <Box my={4}>
-      <PageHeader>
-        <PageHeader.TitleArea>
-          <PageHeader.Title>People</PageHeader.Title>
-        </PageHeader.TitleArea>
-        <PageHeader.Actions>
-          {location.pathname != "/people" && <Button onClick={ () => { navigate("/people")}}>Show all</Button>}
-          <Button variant="primary"
-            data-testid="trigger-button"
-            onClick={() => navigate("/people/new")}
-          >New</Button>
-        </PageHeader.Actions>
-      </PageHeader>
-      <br />
-      <DataTable
-        aria-labelledby="people-table"
-        aria-describedby="people-table-description"
-        columns={[
-          {
-            header: 'Name',
-            field: 'name',
-            rowHeader: true,
-            renderCell: (row: Person) => (
-              <Stack direction="horizontal">
-                <StateIcon state={row.state} />
-                {row.hidden ? <Octicon icon={EyeClosedIcon} /> : <Octicon icon={RepoIcon} />}
+      <TableContainer>
+        <Table.Title id="people">People</Table.Title>
+        <Table.Subtitle id="people-subtitle">All people we are researching</Table.Subtitle>
+        <Table.Actions><IconButton icon={PlusIcon} aria-label="Add" variant="invisible" onClick={() => navigate("/people/new")} />
+          { location.pathname != "/people" && <IconButton icon={ArrowRightIcon} aria-label="Show all" variant="invisible" onClick={() => navigate("/people/new")} />}
+        </Table.Actions>
+        <DataTable
+          aria-labelledby="people-table"
+          aria-describedby="people-table-description"
+          columns={[
+            {
+              header: 'Name',
+              field: 'name',
+              rowHeader: true,
+              renderCell: (row: Person) => (
+                <Stack direction="horizontal">
+                  <StateIcon state={row.state} />
+                  {row.hidden ? <Octicon icon={EyeClosedIcon} /> : <Octicon icon={RepoIcon} />}
+                  <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
+                    {row.name}
+                  </Link>
+                </Stack>
+              ),
+            },
+            {
+              header: 'Birth',
+              field: 'birth',
+              renderCell: (row: Person) => (
                 <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
-                  {row.name}
+                  {row.birth}
                 </Link>
-              </Stack>
-            ),
-          },
-          {
-            header: 'Birth',
-            field: 'birth',
-            renderCell: (row: Person) => (
-              <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
-                {row.birth}
-              </Link>
-            ),
-          },
-          {
-            header: 'Death',
-            field: 'death',
-            renderCell: (row: Person) => (
-              <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
-                {row.death}
-              </Link>
-            ),
-          },
-          {
-            header: 'ID',
-            field: 'id',
-            renderCell: (row: Person) => (
-              <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset', fontFamily: 'monospace' }}>
-                {row.id}
-              </Link>
-            )
-          },
-          {
-            header: 'Created',
-            field: 'created_at',
-            renderCell: (row: Person) => (
-              <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
-                {/* @ts-ignore */}
-                <RelativeTime dateTime={row.created_at} />
-              </Link>
-            )
-          }
-        ]}
-        data={data}
-      />
+              ),
+            },
+            {
+              header: 'Death',
+              field: 'death',
+              renderCell: (row: Person) => (
+                <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
+                  {row.death}
+                </Link>
+              ),
+            },
+            {
+              header: 'ID',
+              field: 'id',
+              renderCell: (row: Person) => (
+                <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset', fontFamily: 'monospace' }}>
+                  {row.id}
+                </Link>
+              )
+            },
+            {
+              header: 'Created',
+              field: 'created_at',
+              renderCell: (row: Person) => (
+                <Link as={RouterLink} to={`/people/${row.id}`} sx={{ color: 'unset' }}>
+                  {/* @ts-ignore */}
+                  <RelativeTime dateTime={row.created_at} />
+                </Link>
+              )
+            }
+          ]}
+          data={data}
+        />
+      </TableContainer>
     </Box>
   );
 
