09ca1b4332fb6c3f88859d9273f822672a71e8ad
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-10-04 15:29:09 +0200

    Added tip for setting username

--- src/components/AppHome.tsx
+++ src/components/AppHome.tsx
@@ -1,4 +1,4 @@
-import { Stack, Heading, Text, Avatar, Box, Octicon } from '@primer/react'
+import { Stack, Heading, Text, Spinner, Box, Octicon, Link } from '@primer/react'
 import { ThreeBarsIcon, FileMediaIcon } from '@primer/octicons-react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
@@ -7,9 +7,11 @@ import WhatsNew from './WhatsNew';
 import { useEffect, useState } from 'react';
 import { User } from '../Interfaces';
 import { supabase } from '../services/supabaseClient';
+import { Banner } from '@primer/react/drafts';
 
 export default function AppHome(props: any) {
   const [user, setUser] = useState<User | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     let ignore = false
@@ -29,7 +31,7 @@ export default function AppHome(props: any) {
           setUser(data)
         }
       }
-
+      setLoading(false)
     }
 
     getProfile()
@@ -38,8 +40,18 @@ export default function AppHome(props: any) {
       ignore = true
     }
   }, [props.session])
+
+  if(loading) {
+    return <Stack direction="horizontal" align="center"><Spinner /><Text>Loading...</Text></Stack>
+  }
   return <Stack>
-    <Heading>Welcome, { user?.username ? user.username : props.session && props.session.user.email }!</Heading>
+    <Heading>Welcome, { user?.username ?? props.session.user.email }!</Heading>
+    {!user?.username && <Banner title="Have you already set a display name?" description={
+      <Text>
+        Click on your Avatar in the top right corner of this page. You can set a display name there.<br/>
+        <Link as={RouterLink} to="/account">Let's see</Link>
+      </Text>
+    } variant="upsell" />}
     <WhatsNew />
     
     <PeopleTable />
