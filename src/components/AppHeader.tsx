2146d0ee4339b4a755d9ae6ba84b8ca4d383dbb5
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-10-04 18:47:41 +0200

    Add sidebar menu (beta)

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,45 +1,26 @@
-import { Button, Header, Heading, Text, Avatar, Link, Octicon } from '@primer/react'
+import { Button, Header, IconButton, Text, Avatar, Link, Octicon } from '@primer/react'
 import { ThreeBarsIcon, FileMediaIcon } from '@primer/octicons-react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import ProfileMenu from './ProfileMenu';
+import Sidebar from './AppSidebar';
+import { useState } from 'react';
+import AppSidebar from './AppSidebar';
 
 export default function AppHeader(props: any) {
+  const [showSidebar, setShowSidebar] = useState(false)
   return <Header>
     <Header.Item>
+      <AppSidebar/>
+    </Header.Item>
+  
+    <Header.Item full>
       <Header.Link as={RouterLink} to="/" mr={2} sx={{ fontSize: 2, }}>
         <Octicon icon={FileMediaIcon} size={32} sx={{ mr: 2, }} />
-        <span>AG Hochstraße</span>
       </Header.Link>
     </Header.Item>
 
-    <Header.Item>
-      <Header.Link sx={{ fontWeight: 'normal' }} as={RouterLink} to="/" >
-        Home
-      </Header.Link>
-    </Header.Item>
-
-    <Header.Item>
-      <Header.Link sx={{ fontWeight: 'normal' }} as={RouterLink} to="/people" >
-        People
-      </Header.Link>
-    </Header.Item>
-
-    <Header.Item>
-      <Header.Link sx={{ fontWeight: 'normal' }} as={RouterLink} to="/about" >
-        About
-      </Header.Link>
-    </Header.Item>
-
-    <Header.Item full >
-      <Header.Link sx={{ fontWeight: 'normal' }} as={RouterLink} to="/whatsnew" >
-        Updates
-      </Header.Link>
-    </Header.Item>
-
-    <Header.Item sx={{ mr: 0, }} >
-      
-      
+    <Header.Item sx={{ mr: 0 }} >
       {/* @ts-ignore */}
       <ProfileMenu session={props.session}/>
     </Header.Item>
