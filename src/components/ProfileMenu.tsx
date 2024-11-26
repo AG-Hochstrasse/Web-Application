f09a622e6df7e1019c4760bb985c740f9dffdc27
CleverLemming1337 <cleverlemming@icloud.com>
2024-11-26 16:40:03 +0100

    Update ProfileMenu.tsx

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ActionMenu, ActionList, Avatar } from "@primer/react";
+import { ActionMenu, ActionList, Avatar, IconButton } from "@primer/react";
 import { FeedPersonIcon } from "@primer/octicons-react";
 
 export default function ProfileMenu() {
   return (
     {/* @ts-ignore */}
-    <FeedPersonIcon onClick={() => { window.location.href = "/account" }} />
+    <IconButton icon={FeedPersonIcon} onClick={() => { window.location.href = "/account" }} />
   )
 }
